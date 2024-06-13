af4669d32d67f980f48ecd1b010fbaffb05b1124
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2024-06-12 23:21:58 -0300

    Add score variables

--- js/rock-paper-scissors/scripts/script.js
+++ js/rock-paper-scissors/scripts/script.js
@@ -1,4 +1,6 @@
 const answers = ['rock', 'paper', 'scissors'];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * answers.length);
